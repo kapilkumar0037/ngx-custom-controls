cb25aff63235ebf6cee75cc83ee101a3e3c105f2
Kapil Kumar <kapilkumar0037@gmail.com>
2025-01-15 21:14:04 +0530

    restructuring

--- projects/ngx-custom-controls/src/custom-datepicker/component/custom-datepicker.component.ts
+++ projects/ngx-custom-controls/src/custom-datepicker/component/custom-datepicker.component.ts
@@ -2,7 +2,7 @@ import { Component, forwardRef, input } from '@angular/core';
 import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { BaseCvaImplementationDirective } from '../../shared/directives/base-cva-implementation.directive';
 import { NgClass } from '@angular/common';
-import { cvaProviders } from '../../utils/src/providers/cva-providers';
+import { cvaProviders } from '../../shared/providers/cva-providers';
 
 @Component({
   selector: 'ngo-custom-datepicker',
--- projects/ngx-custom-controls/src/custom-radio/component/custom-radio.component.ts
+++ projects/ngx-custom-controls/src/custom-radio/component/custom-radio.component.ts
@@ -2,7 +2,7 @@ import { Component, forwardRef, input } from '@angular/core';
 import { BaseCvaImplementationDirective } from '../../shared/directives/base-cva-implementation.directive';
 import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { NgClass } from '@angular/common';
-import { cvaProviders } from '../../utils/src/providers/cva-providers';
+import { cvaProviders } from '../../shared/providers/cva-providers';
 
 @Component({
   selector: 'ngo-custom-radio',
