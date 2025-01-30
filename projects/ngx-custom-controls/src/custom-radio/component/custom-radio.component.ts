cbc8604e60dde2baeeb36c645477e19523ad6903
Kapil Kumar <kapilkumar0037@gmail.com>
2025-01-30 21:53:20 +0530

    controls implementation

--- projects/ngx-custom-controls/src/custom-radio/component/custom-radio.component.ts
+++ projects/ngx-custom-controls/src/custom-radio/component/custom-radio.component.ts
@@ -2,6 +2,7 @@ import { Component, input } from '@angular/core';
 import { BaseCvaImplementationDirective } from '../../shared/directives/base-cva-implementation.directive';
 import { NgClass } from '@angular/common';
 import { cvaProviders } from '../../shared/providers/cva-providers';
+import { BaseCvaImplementationDirective } from '../../custom-checkbox/base-cva-implementation.directive';
 
 @Component({
   selector: 'ngcc-custom-radio',
